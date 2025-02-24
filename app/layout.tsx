2603bdd57d0075e7a82f7975d70c6944a0d08443
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-24 15:56:13 +0330

    working on themes (dark and light)

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${vazir.className} antialiased`}
+        className={`${vazir.className} antialiased dark`}
       >
         <Navbar />
         {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'yellow-1': "rgba(var(--yellow1))",
+        'yellow-2': "rgba(var(--yellow2))",
+        'yellow-3': "rgba(var(--yellow3))",
+        'gray-1': "rgba(var(--gray1))",
+        'gray-2': "rgba(var(--gray2))",
+        'gray-3': "rgba(var(--gray3))",
+        blue : "rgba(var(--blue))"
       },
     },
   },
