529b0e0840db64026dab9005360e000df16f002f
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-26 22:17:45 +0330

    a little change

--- app/api/movie/hottest/route.ts
+++ app/api/movie/hottest/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import pool from "@/app/assets/db/pool";
 
 export async function GET() {
-  const query = `SELECT * FROM movie WHERE title != 'unknown' ORDER BY rating DESC LIMIT 8`;
+  const query = `SELECT DISTINCT * FROM movie WHERE title != 'unknown' ORDER BY rating DESC LIMIT 8`;
   try {
     const { rows } = await pool.query(query);
     return NextResponse.json(rows, { status: 200 });
