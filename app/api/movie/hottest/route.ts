77850c43cd2ba9867eb3f5676e985da62a59a7de
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-26 21:23:59 +0330

    working on hottest endpoint (GET).

---
+++ app/api/movie/hottest/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import pool from "@/app/assets/db/pool";
+
+export async function GET() {
+  const query = `SELECT * FROM movie WHERE title != 'unknown' ORDER BY rating DESC LIMIT 8`;
+  try {
+    const { rows } = await pool.query(query);
+    return NextResponse.json(rows, { status: 200 });
+  } catch (e) {
+    console.log("Error in getting data from pool:\n" + e);
+    return NextResponse.json("internal server error.", { status: 500 });
+  }
+}
