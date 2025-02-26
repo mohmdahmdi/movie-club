eaf879bcc0332b1f7cfe885ed3c32c482492fbb7
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-26 22:18:50 +0330

    using hottest section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "./components/heroSection/heroSection";
+import HottestSection from './components/hottestSection/hottestSection';
 export default function Home() {
   return (
     <div className="h-[200vh]">
       <HeroSection />
+      <HottestSection />
     </div>
   );
 }
